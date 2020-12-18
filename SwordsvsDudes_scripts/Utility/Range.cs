64ddf12ce577e01ba3a5d57f37aa4f652b87bde7
pbates243 <58965331+pbates243@users.noreply.github.com>
2020-12-18 08:35:00 -0500

    Add files via upload
    Final project for a class on Unity in which I created my own 2D platformer.

---
+++ SwordsvsDudes_scripts/Utility/Range.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Range
+{
+    public float min;
+    public float max;
+
+    public float GetRandomRange ()
+    {
+        return Random.Range(min, max);
+    }
+
+    public Range (float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
